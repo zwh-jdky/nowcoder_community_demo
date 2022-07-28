dde0d18b451a7863305d9069f5e06d4bb78aabaf
jdky <jdky753951@163.com>
2022-07-29 00:32:18 +0800

    项目提交测试

---
+++ src/test/java/com/nowcoder/community/MailTests.java
@@ -0,0 +1,47 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.util.MailClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+/**
+ * @Description
+ * @Author 街道口译
+ * @Date 2022/7/28 23:48
+ * @Version 1.0
+ **/
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MailTests {
+	
+	@Autowired
+	private MailClient mailClient;
+	
+	@Autowired
+	private TemplateEngine templateEngine;
+	
+	@Test
+	public void testTextMail() {
+		mailClient.sendMail("jdky753951@163.com", "TEST", "Welcome.");
+	}
+	
+	@Test
+	public void testHtmlMail() {
+		Context context = new Context();
+		context.setVariable("username", "sunday");
+		
+		String content = templateEngine.process("/mail/demo", context);
+		System.out.println(content);
+		
+		mailClient.sendMail("1937515495@qq.com", "HTML", content);
+	}
+	
+}
